6a249d59b2560b57e36a46da56785d24d0e1452a
Samuel Oliveira Lopes <99331928+Samuskox@users.noreply.github.com>
2022-09-05 09:49:36 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Fizemo =D</Text>
+      <Button
+        title='CLIQUE AQUI PARA CRIAR O ITEM'
+        onPress={()=>{console.log("cllica")}}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
